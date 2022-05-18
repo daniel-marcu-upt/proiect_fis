a0ede5c4f24905953ddd219fff84b0e19f248e30
Daniel Marcu <daniel.marcu@student.upt.ro>
2022-05-18 16:53:14 +0300

    PF-5: test pentru login

--- ConvocareMuncitori/src/test/java/org/loose/fis/sre/B_LoginTest.java
+++ ConvocareMuncitori/src/test/java/org/loose/fis/sre/B_LoginTest.java
@@ -3,17 +3,12 @@ package org.loose.fis.sre;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.robot.Robot;
 import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.loose.fis.sre.controllers.LoginController;
-import org.loose.fis.sre.controllers.RegistrationController;
-import org.loose.fis.sre.model.User;
 import org.loose.fis.sre.services.FileSystemService;
 import org.loose.fis.sre.services.UserService;
 import org.testfx.api.FxRobot;
@@ -21,12 +16,11 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
 
 import static org.testfx.assertions.api.Assertions.assertThat;
 
 @ExtendWith(ApplicationExtension.class)
-class LoginTest {
+class B_LoginTest {
 
     public static final String EMAIL = "muncitor@gmail.com";
     public static final String PASSWORD0 = "password0";
@@ -37,11 +31,6 @@ class LoginTest {
 
     @BeforeAll
     static void setUp() throws Exception {
-        FileSystemService.APPLICATION_FOLDER = ".test-registration-example";
-        FileSystemService.APPLICATION_HOME_PATH = Paths.get(FileSystemService.USER_FOLDER, FileSystemService.APPLICATION_FOLDER);
-        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
-        FileSystemService.initDatabase();
-        UserService.init();
     }
 
     @Start
